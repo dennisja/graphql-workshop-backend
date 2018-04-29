5acb35854ef8647b7113f5c681d56f3d054e5456
dennisja <dennisjjagwe@gmail.com>
2018-04-29 20:25:15 +0300

    Add links model

--- linkmanager/settings.py
+++ linkmanager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'links',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'linkmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'link_manager',
+        'USER':'postgres',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
+        'PORT': '5432'
     }
 }
 
