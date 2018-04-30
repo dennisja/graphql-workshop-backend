3c00bd8b048b882382439c4cd46cdbcb6e379f6a
dennisja <dennisjjagwe@gmail.com>
2018-04-30 10:17:56 +0300

    Prepare application for deployment

--- linkmanager/settings.py
+++ linkmanager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,9 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'link_manager',
-        'USER':'postgres',
-        'PASSWORD':'password',
-        'HOST':'127.0.0.1',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
         'PORT': '5432'
     }
 }
@@ -135,3 +137,5 @@ AUTHENTICATION_BACKENDS = [
     'graphql_jwt.backends.JSONWebTokenBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+
+django_heroku.settings(locals())
