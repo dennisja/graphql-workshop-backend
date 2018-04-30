b41f32258eb2f3f0deb0c8fd39fd6c529d282f07
dennisja <dennisjjagwe@gmail.com>
2018-04-30 10:12:43 +0300

    Add posted ny to the link thing

--- links/models.py
+++ links/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+
 class Link(models.Model):
     url = models.URLField()
     title = models.TextField(blank=True)
-    
+    created_at = models.DateTimeField(default=timezone.now)
+    last_edited = models.DateTimeField(auto_now=True)
+    posted_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+     
--- links/schema.py
+++ links/schema.py
@@ -7,43 +7,62 @@ from .models import Link
 """
 The link type
 """
+
+
 class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
+
 """
 Link mutation fields
 """
+
+
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
     title = graphene.String()
+    last_edited = graphene.DateTime()
+    created_at = graphene.DateTime()
+    posted_by = graphene.Field(LinkType)
 
     class Arguments:
         url = graphene.String()
         title = graphene.String()
 
     def mutate(self, info, url, title):
-        link = Link(url=url, title=title)
+        user = info.contextl.user or None
+
+        link = Link(url=url, title=title, posted_by=user)
         link.save()
 
         return CreateLink(
             id=link.id,
             url=link.url,
-            title=link.title
+            title=link.title,
+            posted_by=link.posted_by,
+            created_at=link.created_at,
+            last_edited=link.last_edited,
         )
+
 
 """
 Queries of link type
 """
+
+
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
 
+
 """
 Mutations of link type
 """
+
+
 class Mutation(graphene.ObjectType):
     create_link = CreateLink.Field()
